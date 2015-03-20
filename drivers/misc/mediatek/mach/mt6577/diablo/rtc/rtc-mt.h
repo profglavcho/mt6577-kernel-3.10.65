f13ba425deb638667c657df1ea7fc40f42f001a2
profglavcho <xpi100@mail.ru>
2015-03-20 14:18:00 +0200

    add

---
+++ drivers/misc/mediatek/mach/mt6577/diablo/rtc/rtc-mt.h
@@ -0,0 +1,13 @@
+
+#ifndef _RTC_MT_H_
+#define _RTC_MT_H_
+
+#define RTC_YES		1
+#define RTC_NO		0
+
+#define RTC_OVER_TIME_RESET	RTC_YES
+#define RTC_DEFAULT_YEA		2012
+#define RTC_DEFAULT_MTH		1
+#define RTC_DEFAULT_DOM		1
+
+#endif
